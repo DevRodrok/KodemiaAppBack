2880c3209ad06fe36c872a7ac608ed2e22c87e2f
DevRodrok <rodzg7@gmail.com>
2021-07-03 13:03:24 -0500

    login y signUp funcionando para revision x2

--- src/models/admin.js
+++ src/models/admin.js
@@ -29,7 +29,7 @@ const schema = new mongoose.Schema({
  },
  picture: {
    type: String,
-   required: true
+   required: false
  }
 
  
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -20,11 +20,11 @@ router.post('/koders/signIn', async (req, res)=>{
 
   try {
     const {lastName, firstName, generation, email, password, isActive, bootcamp, phone, picture} = req.body
-    const adminCreated = await auth.signUpKoders(lastName, firstName, generation, email, password, isActive, bootcamp, phone, picture)
+    const koderCreated = await auth.signUpKoders(lastName, firstName, generation, email, password, isActive, bootcamp, phone, picture)
 
     res.json({
       success: true,
-      data: adminCreated
+      data: koderCreated
     })
   } catch(error){
     res.status(401)
@@ -52,6 +52,48 @@ router.post('/koder/login', async(req, res)=>{
     res.status(401)
     res.json({
       success: false,
+      message: error.message
+    })
+  }
+})
+
+router.post('/admin/signIn', async (req, res) => {
+
+try {
+  const {lastName, firstName, email, password, phone, picture} = req.body
+  const adminCreated = await auth.signUpAdmins(lastName, firstName, email, password, phone, picture )
+
+  res.json({
+    success: true,
+    data: adminCreated
+  })
+} catch(error) {
+  res.status(401)
+  res.json({
+    success: false,
+    message: error.message
+  })
+}
+
+})
+
+router.post('/admin/login', async(req, res) => {
+  try {
+    const {email, password} = req.body
+    const token = await auth.loginAdmins(email, password)
+
+    res.json({
+      success: true,
+      message: ('Admin logged'),
+      data: {
+        token: token,
+        email: email
+      }
+    })
+  } catch(error){
+    res.status(401)
+    res.json({
+      success: false, 
       message: error.message
     })
   }
